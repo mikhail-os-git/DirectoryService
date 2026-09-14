71da0559a8cead2ae54ae8331c89c5e5d8ccf650
Mikhail Osipov <mishaend@gmal.com>
2026-09-14 17:01:01 +0200

    Fix Failure

--- DirectoryService/src/DirectoryService.Application/Departments/GetAllDepartments/GetAllDepartmentsValidator.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/GetAllDepartments/GetAllDepartmentsValidator.cs
@@ -14,38 +14,52 @@ public class GetAllDepartmentsValidator: AbstractValidator<GetAllDepartmentsRequ
 
         RuleFor(x => x.PageSettings)
             .NotNull()
-            .WithError(Failure.Validation("page.settings.null", "Page settings required"));
+            .WithError(Failure.Validation(
+                "Page settings required",
+                "page.settings.null"));
         
         RuleFor(x => x.PageSettings.Page)
             .NotNull()
-            .WithError(Failure.Validation("page.number.null", "Page number must be not null"))
+            .WithError(Failure.Validation(
+                "Page number must be not null",
+                "page.number.null"))
             .When(x => x.PageSettings is not null)
             .GreaterThan(0)
-            .WithError(Failure.Validation("page.number.negative", "Page number must be greater than 0"));
+            .WithError(Failure.Validation(
+                "Page number must be greater than 0",
+                "page.number.negative"));
         
         RuleFor(x => x.PageSettings.PageSize)
             .NotNull()
-            .WithError(Failure.Validation("page-size.number.null", "Page Size number must be not null"))
+            .WithError(Failure.Validation(
+                "Page Size number must be not null",
+                "page-size.number.null"))
             .When(x => x.PageSettings is not null)
             .GreaterThan(0)
-            .WithError(Failure.Validation("page-size.number.negative", "Page Size number must be greater than 0"))
+            .WithError(Failure.Validation(
+                "Page Size number must be greater than 0",
+                "page-size.number.negative"))
             .LessThanOrEqualTo(100)
-            .WithMessage("Page size must be between 1 and 100");
+            .WithError(Failure.Validation(
+                "Page size must be between 1 and 100", 
+                "page-size.number.negative"));
         
         RuleFor(x => x.Search)
             .MaximumLength(500)
-            .WithError(Failure.Validation("search.query.too-long", "Search query cannot exceed 500 characters"));
+            .WithError(Failure.Validation(
+                "Search query cannot exceed 500 characters",
+                "search.query.too-long")); 
         
         RuleFor(x => x.SortBy)
             .Must(name => allowSortName.Contains(name))
             .WithError(Failure.Validation(
-                "department.sort-by.invalid",
-                $"Invalid sortBy value. Allowed values: {string.Join(", ", allowSortName)}"));
+                $"Invalid sortBy value. Allowed values: {string.Join(", ", allowSortName)}",
+                "department.sort-by.invalid"));
         
         RuleFor(x => x.SortDir)
             .Must(dir => allowSortDir.Contains(dir))
             .WithError(Failure.Validation(
-                "department.sort-dir.invalid",
-                $"Invalid sort-direction value. Allowed values: {string.Join(", ", allowSortDir)}"));
+                $"Invalid sort-direction value. Allowed values: {string.Join(", ", allowSortDir)}",
+                "department.sort-dir.invalid"));
     }
 }
